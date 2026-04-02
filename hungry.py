b40bf0482bf87ebb1b2c5fef811b14dba97191fb
fiyin <fiyinowoade2005@gmail.com>
2026-04-02 14:44:52 +0100

    latest push

--- hungry.py
+++ hungry.py
@@ -1,4 +1,11 @@
-x=int(input("Enter number"))
-y=int(input("Enter number"))
-total=x+y
-print(total)
+operation=input("what operation would you to peform?: ")
+if operation== "add":
+    x=int(input("Enter number"))
+    y=int(input("Enter number"))
+    total=x+y
+    print(f"total is {total}")
+else:
+    x = int(input("Enter number"))
+    y = int(input("Enter number"))
+    total = x - y
+    print(f"total is {total}")
